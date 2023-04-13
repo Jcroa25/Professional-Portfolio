f7a812a36381cde808aef8b67d3a147b37c8a986
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-04-13 15:42:34 -0400

    added a submission box

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+const sendButton = document.getElementById('send')
+
+
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
+
 hamburgerButton.addEventListener('click', toggleButton)
+
+
