f9d2bea3dac4ec1c33ee1166863d93425ec97a67
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-04-13 15:49:59 -0400

    code cleanup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
-const sendButton = document.getElementById('send')
-
-
 
 function toggleButton() {
     navList.classList.toggle('show')
